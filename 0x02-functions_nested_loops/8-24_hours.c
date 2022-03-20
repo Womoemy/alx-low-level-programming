57dfa7a517017490ce50ad84337beea5d673a9e3
Womoemy <womoemiebiobowei@gmail.com>
2022-03-20 15:10:16 -0700

    print every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,8 +3,7 @@
 /**
  * jack_bauer(void) - print every minute of the day
  *
- * Description: function that prints every minute of the day of Jack Bauer,
- * starting from 00:00 to 23:59
+ * Description: function that prints every minute of the day of Jack Bauer
  * Return: Always 0 success
  */
 void jack_bauer(void)
