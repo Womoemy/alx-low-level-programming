fa4589b0b13f91316037cbd8d87c9158b3647a39
Womoemy <womoemiebiobowei@gmail.com>
2022-03-21 07:09:35 -0700

    function to update pointer value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,8 @@
  * reset_to_98 - int pointer with value 98
  * @n: pointer to int n
  *
- * Description: function that takes an int pointer as parameter and points to 98
+ * Description: function that takes an int pointer as parameter 
+ * and points to 98
  */
 void reset_to_98(int *n)
 {
